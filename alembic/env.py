c998e0ee5de7039e0a9004f53410ee84854b8ce6
prokopevaeg <prokopeva@yandex.ru>
2026-05-02 10:27:13 +0500

    Состояние с проблемоц доступа к БД

--- alembic/env.py
+++ alembic/env.py
@@ -11,7 +11,7 @@ from nlp.tables import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-config = context.get_config()
+config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
